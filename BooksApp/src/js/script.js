ae62cef74b69f00e932fd62671aab19580a3933a
Alicja Jagielska <alicja_dubai@wp.pl>
2022-02-18 13:55:04 +0100

    Poprawki

--- BooksApp/src/js/script.js
+++ BooksApp/src/js/script.js
@@ -8,52 +8,51 @@
   };
   const template = {
     book: Handlebars.compile(
-      document.querySelector(select.templateOf.books).innerHTML
+      document.querySelector(select.templateOf.book).innerHTML
     ),
   };
-  const bookList = document.querySelectorAll("book-list");
-  console.log(bookList);
-
-  const favoriteBooks = [];
 
   function renderInBooks() {
     for (let book of dataSource.books) {
-      bookDetails = {
+      const bookDetails = {
         id: book.id,
         name: book.name,
         price: book.price,
         rating: book.rating,
         image: book.image,
       };
-      console.log(bookDetails);
+      console.log(bookList);
       // const generatedHTML = templates.menuProduct(thisProduct.data);
       const generatedHTML = template.book(bookDetails);
       // /* create element using utils.createElementFromHTML */
       const generatedDOM = utils.createDOMFromHTML(generatedHTML);
       // /* add element to menu */
-      bookList.appendChild(generetedDOM);
+      console.log(bookList);
+      bookList.appendChild(generatedDOM);
     }
   }
   function initAction() {
-    const bookImages = bookList.querySelectorAll("book-image");
-    console.log(bookImages);
-    const clickedElement = this;
-
-    for (let bookImage of bookImages) {
-      bookImage.addEventListener("dblClick", function (event) {
-        event.preventDefault();
-        const clickedElement = event.target.offsetParent;
-        if (clickedElement.classList.contains("book__image")) {
-          const dataId = clickedElement.getAttribute("data-id");
-          if (favoriteBooks.includes(dataId)) {
-            clickedElement.classList.toggle("favorite");
-          } else {
-            console.log(dataId);
-            favoriteBooks.push(dataId);
-          }
+    const favoriteBooks = [];
+    console.log("fav books", favoriteBooks);
+    const bookList = document.querySelector(".books-list");
+    console.log("aa", bookList);
+    //const bookImages = bookList.querySelectorAll(".book-image");
+    // console.log("book images", bookImages);
+    //const clickedElement = this;
+    bookList.addEventListener("click", function (event) {
+      event.preventDefault();
+      const clickedElement = event.target.offsetParent;
+      console.log(clickedElement);
+      if (clickedElement.classList.contains("book__image")) {
+        clickedElement.classList.toggle("favorite");
+        const dataId = clickedElement.getAttribute("data-id");
+        if (!favoriteBooks.includes(dataId)) {
+          favoriteBooks.push(dataId);
+        } else {
+          favoriteBooks.splice(favoriteBooks.indexOf, 1);
         }
-      });
-    }
+      }
+    });
   }
 
   renderInBooks();
